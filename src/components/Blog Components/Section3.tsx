a0f8f786081766a57132c172a21350c21ca15111
Derrick <pshubomi@gmail.com>
2025-02-19 10:15:21 +0100

    done working on section 3 for the blog page

--- src/components/Blog Components/Section3.tsx
+++ src/components/Blog Components/Section3.tsx
@@ -11,15 +11,15 @@ function Section3() {
         backgroundPosition: "center",
       }}
     >
-      <div className="flex flex-col justify-between p-8 h-full">
+      <div className="flex flex-col justify-between p-8 h-full max-[420px]:p-0">
         <div className="border-2 border-white py-1 px-2 text-white z-50 self-start text-sm">
           SUNDAY SERVICE
         </div>
-        <div className="z-50">
+        <div className="z-50 w-2/3 max-[750px]:w-full">
           <span className="flex gap-2 text-sm items-center text-white">
             <HiOutlineCalendarDateRange /> Nov 5 - <CiTimer /> 20mins read
           </span>
-          <h2 className="text-4xl font-extrabold text-white my-4">
+          <h2 className="text-4xl max-[700px]:text-2xl font-extrabold max-[700px]:font-semibold text-white my-4">
             The Power of Worship: How Praising God Transforms Your Life
           </h2>
           <p className="text-gray-300 text-sm pt-4">
