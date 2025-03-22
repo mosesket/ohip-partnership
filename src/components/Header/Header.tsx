ccbcfbd3abf105a5827b929e8c859f6d82faea19
Moses Ketuojo <sirketxp@gmail.com>
2025-03-22 16:38:14 +0100

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,7 +14,7 @@ function Header() {
   };
   return (
     <header className="bg-white shadow-md p-4 px-8 flex justify-between items-center fixed w-full z-[1000] h-24">
-      <img src={logo} alt="Ohip LOgo" className="w-25 -rotate-90" />
+      <img src={logo} alt="Ohip LOgo" className="w-25 " />
       <nav className="hidden min-[860px]:block">
         <ul className="flex gap-10 text-lg">
           <li>
